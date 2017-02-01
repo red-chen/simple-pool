16775cbc0514806c190d26df6ad44312c7aa00c1
redchen <redchen1255@gmail.com>
2017-01-31 19:30:50 -0500

    add test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+file_list=`find bin -name "*_unittest"`
+
+flag=0
+
+for file_path in $file_list
+do 
+    valgrind --track-origins=yes --error-exitcode=42 --leak-check=full --show-reachable=yes $file_path
+    [ $? != 0 ] && flag=1
+done
+
+[ $flag != 0 ] && echo "test fail."
+
+exit $flag
